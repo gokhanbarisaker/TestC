2b625b1775c1dcf76ef6dfd2b367c72dfe4e1940
MonkeyPerformingCodingAction <gokhan.05@gmail.com>
2011-07-30 16:56:49 +0300

    Created an empty project

---
+++ TestCProject/src/com/clockwork/testc/TestCProject.java
@@ -0,0 +1,13 @@
+package com.clockwork.testc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestCProject extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
